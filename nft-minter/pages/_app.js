2aa5b766c55792ee2dc858a5f2f6d9946c073c50
Ammar-Alraddadi <78421734+Ammar-Alraddadi@users.noreply.github.com>
2023-01-11 13:59:47 +0300

    Add files via upload

---
+++ nft-minter/pages/_app.js
@@ -0,0 +1,22 @@
+// import '../styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
+function MyApp({ Component, pageProps }) {
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
+}
+export default MyApp;
